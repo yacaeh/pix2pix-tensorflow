6a5e634ff881f1980ca6311dc957d90ab9dac98e
shoh <shoh4486@naver.com>
2019-11-01 19:39:27 +0900

    commit v1

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ def mkdir(directory):
         if not os.path.exists(directory):
             os.makedirs(directory)     
     except OSError:
-        raise OSError('Cannot make the directory "{0}"'.format(directory))
+        print('Cannot make the directory "{0}"'.format(directory))
     
 def global_variables_list():
     return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
