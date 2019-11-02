41184f23f064b6e9c34d41105071321d80b58000
shoh <shoh4486@naver.com>
2019-11-02 20:42:24 +0900

    commit v1

--- utils.py
+++ utils.py
@@ -19,3 +19,5 @@ def global_variables_list():
 def pixel_checker(image):
     if (np.min(image) < -1) or (np.max(image) > 1):
         raise ValueError('Pixel values should be in -1~1 range.')
+    else:
+        pass
